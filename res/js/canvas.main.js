742e4f69f00086b244a3a1e7a72e5cd56cb4b242
Jong Kyu, Lim <jklim1253@gmail.com>
2016-01-12 01:55:05 +0900

    canvas 파일들 추가.

---
+++ res/js/canvas.main.js
@@ -0,0 +1,56 @@
+// Canvas main
+
+window.addEventListener("load", eventWindowLoaded, false);
+
+var Debugger = function() {};
+Debugger.log = function(msg) {
+	try {
+		console.log(msg);
+	}
+	catch (exception) {
+		return;
+	}
+}
+
+function eventWindowLoaded() {
+	MainApp();
+}
+function canvasSupport() {
+	return Modernizr.canvas;
+}
+function MainApp() {
+	if (!canvasSupport) {
+		return;
+	}
+
+	var canvas = document.getElementById("screen");
+	var context = canvas.getContext("2d");
+
+	Debugger.log("Drawing Canvas");
+
+	function drawScreen() {
+
+		// fill background.
+		context.fillStyle = "#ffffaa";
+		context.fillRect(0,0,500,300);
+
+		// draw text
+		context.fillStyle = "#000000";
+		context.font = "20px Sans-Serif";
+		context.textBaseline = "top";
+		context.fillText("Hello World!", 195, 80);
+
+		// draw image
+		var helloImage = new Image();
+		helloImage.onload = function() {
+			context.drawImage(helloImage, 145, 90);
+		}
+		helloImage.src = "res/img/hello.png";
+
+		// draw box
+		context.strokeStyle = "#ffaaaa";
+		context.strokeRect(10, 10, 480, 280);
+	}
+
+	drawScreen();
+}
